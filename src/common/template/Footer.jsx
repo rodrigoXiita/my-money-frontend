966ca4a2f4210f64281ad55bc8fefa9e11263435
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-08-11 21:57:28 -0300

    init

---
+++ src/common/template/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2017
+      <a href='http:rodrigo' target='_blank'>Rodrigo</a>.
+    </strong>
+  </footer>
+)
