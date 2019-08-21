52e190497a7bd341c9045f0d578a4d4f5d059d15
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-08-21 10:02:14 -0300

    Inserir botão cancelar

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {init} from './billingCycleActions'
 import {reduxForm, Field} from 'redux-form'
 import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
   render(){
-    const {handleSubmit} = this.props
+    const {handleSubmit, init} = this.props
     return (
       <form role='form' onSubmit={handleSubmit}>
         <div className='box-body'>
@@ -17,10 +20,16 @@ class BillingCycleForm extends Component {
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
+            <button type='button' className='btn btn-default' onClick={init}>Cancelar</button>
         </div>
       </form>
     )
   }
 }
 
-export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const mapDispatchToProps = dispatch => bindActionCreators(
+    {init},
+    dispatch
+)
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
