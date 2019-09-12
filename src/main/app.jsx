fa3415de7bd935b250cd020d32baa0fa219c0411
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-09-12 10:47:43 -0300

    update React 16

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import {HashRouter} from 'react-router-dom'
+
 import Header from '../common/template/header';
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/Footer'
 import Messages from '../common/msg/messages'
 
+import Routes from './routes'
+
 export default props => (
-  <div className='wrapper'>
-    <Header/>
-    <SideBar/>
-    <div className='content-wrapper'>
-        {props.children}
-    </div>
-    <Footer/>
-    <Messages/>
-  </div>
+    <HashRouter>
+      <div className='wrapper'>
+        <Header/>
+        <SideBar/>
+            <Routes/>
+        <Footer/>
+        <Messages/>
+      </div>
+    </HashRouter>
 )
